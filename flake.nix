7a5952bd8e6b3ed980c9eb7160172a2b7cc95437
Archisman-Mridha <archismanmridha12345@gmail.com>
2025-07-04 09:09:04 +0530

    chore : development environment setup using nix flake
    Signed-off-by: Archisman-Mridha <archismanmridha12345@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Authentication Microservice development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            go_1_24
+            golangci-lint
+            golines
+
+            protobuf
+            buf
+
+            sqlc
+          ];
+
+          buildInputs = [ go-task ];
+        };
+      });
+}
